3827bbd97327fd051d0a1f4696a8353c6eeb18ac
sarthakwai <sarthak@wadhwaniai.org>
2024-10-15 19:59:24 +0530

    added model for weather analytics

--- ctva_weather_app/weather/models.py
+++ ctva_weather_app/weather/models.py
@@ -6,6 +6,14 @@ class WeatherData(models.Model):
     # id field is auto generated
     station_id = models.CharField(max_length=99)
     date = models.DateField()
-    max_temperature = models.IntegerField(null=True)
-    min_temperature = models.IntegerField(null=True)
-    precipitation = models.IntegerField(null=True)
+    max_temperature = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+    min_temperature = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+    precipitation = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+
+
+class WeatherAnalytics(models.Model):
+    station_id = models.CharField(max_length=99)
+    year = models.IntegerField()
+    avg_max_temp = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+    avg_min_temp = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+    total_ppt = models.DecimalField(max_digits=5, decimal_places=2, null=True)
